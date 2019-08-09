04834a61ef11ad53027e86d4686ebbf97d1bbecc
Theresa <s79136@informatik.htw-dresden.de>
2019-08-09 16:05:05 +0200

    _

--- BST.cpp
+++ BST.cpp
@@ -1,5 +1,40 @@
-#include "BST.h"
+#ifndef BST_H
+#define BST_H
+
+#include <iostream>
+#include <list>
 #include <cmath>
+
+using namespace std;
+
+template <class T, class K>
+using getKey = K (*)(const T&);
+
+template <class K>
+using cmpKey = int (*)(const K&, const K&);
+
+template <class T, class K>
+class BST{
+	public:
+	BST(getKey<T,K> gK, T data, cmpKey<K> cK);
+	~BST();
+	void setLeftChild(BST<T, K> *pBinTree){pLeft=pBinTree;}
+	void setRightChild(BST<T, K> *pBinTree){pRight=pBinTree;}
+	BST<T, K> *getLeftChild()const{return pLeft;}
+	BST<T, K> *getRightChild()const{return pRight;}
+	void insert(T newdata);
+	void insert(list<T> dataList);
+	T *find(K key);
+	template <class U, class V>
+	friend ostream &operator <<(ostream &OS, const BST<U, V> &bst);
+	private:
+	BST<T, K> *pLeft=NULL;
+	BST<T, K> *pRight=NULL;
+	T data;
+	K m_key;
+	getKey<T,K> gK; // function that takes data as an argument and returns its key
+	cmpKey<K> cK; // function that compares two keys
+};
 
 template <class T, class K>
 BST<T,K>::BST(getKey<T,K> gK, T data, cmpKey<K> cK):data{data}, m_key{(*gK)(data)}, cK{cK}, gK{gK}{
@@ -74,3 +109,5 @@ ostream &operator<<(ostream &OS, const BST<U, V> &bst){
 	}
 	return OS;
 }
+
+#endif
